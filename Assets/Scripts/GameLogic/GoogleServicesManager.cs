c70be2f1c1b7d30499840b2a694672f34f67f245
Emanuel <bh19hr@student.sunderland.ac.uk>
2021-05-14 16:25:58 +0300

    Implementing Authentication with Google Play Services
    This commit contains the files that create the connection to google play services. The game manager holds the API connection and is triggered by the user interface script. GPGIds is the autogenerated file that contains the id of the achievements, leaderboard and potential events from Google Play Services.

---
+++ Assets/Scripts/GameLogic/GoogleServicesManager.cs
@@ -0,0 +1,188 @@
+﻿namespace GameLogic
+{
+    using UnityEngine;
+    using System.Collections.Generic;
+    using GooglePlayGames;
+    using GooglePlayGames.BasicApi.SavedGame;
+    using System;
+    using GooglePlayGames.BasicApi;
+
+    public class GoogleServicesManager
+    {
+        private static GoogleServicesManager sInstance = new GoogleServicesManager();
+
+        private bool mAuthenticating = false;
+        private string mAuthProgressMessage = Strings.SigningIn;
+
+        // achievement increments we are accumulating locally, waiting to send to the games API
+        private Dictionary<string, int> mPendingIncrements = new Dictionary<string, int>();
+
+        // list of achievements we know we have unlocked (to avoid making repeated calls to the API)
+        private Dictionary<string, bool> mUnlockedAchievements = new Dictionary<string, bool>();
+
+        // what is the highest score we have posted to the leaderboard?
+        private int mHighestPostedScore = 0;
+
+        public static GoogleServicesManager Instance
+        {
+            get { return sInstance; }
+        }
+
+        void ReportAllProgress()
+        {
+            FlushAchievements();
+            //UnlockProgressBasedAchievements();
+            PostToLeaderboard();
+            //AutoSave();
+        }
+
+        public void FlushAchievements()
+        {
+            if (Authenticated)
+            {
+                foreach (string ach in mPendingIncrements.Keys)
+                {
+                    // incrementing achievements by a delta is a feature
+                    // that's specific to the Play Games API and not part of the
+                    // ISocialPlatform spec, so we have to break the abstraction and
+                    // use the PlayGamesPlatform rather than ISocialPlatform
+                    PlayGamesPlatform p = (PlayGamesPlatform)Social.Active;
+                    p.IncrementAchievement(ach, mPendingIncrements[ach], (bool success) => { });
+                }
+
+                mPendingIncrements.Clear();
+            }
+        }
+
+        internal void GoToScene(string scene)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+        }
+
+        public void RestartLevel()
+        {
+            //AutoSave();
+            ReportAllProgress();
+            GoToScene("2_GameplayScene");
+        }
+
+        public void FinishLevelAndGoToNext(int score, int stars)
+        {
+            //mProgress.SetLevelProgress(mLevel, score, stars);
+            //AutoSave();
+            ReportAllProgress();
+            //TO DO - work on this
+        }
+
+        public void QuitToMenu()
+        {
+            //AutoSave();
+            ReportAllProgress();
+            GoToScene("1_MenuScene");
+        }
+
+        public void UnlockAchievement(string achId)
+        {
+            if (Authenticated && !mUnlockedAchievements.ContainsKey(achId))
+            {
+                Social.ReportProgress(achId, 100.0f, (bool success) => { });
+                mUnlockedAchievements[achId] = true;
+            }
+        }
+
+
+        public bool Authenticating
+        {
+            get { return mAuthenticating; }
+        }
+
+        public bool Authenticated
+        {
+            get { return Social.Active.localUser.authenticated; }
+        }
+
+        public void Authenticate()
+        {
+            if (Authenticated || mAuthenticating)
+            {
+                Debug.LogWarning("Ignoring repeated call to Authenticate().");
+                return;
+            }
+
+            // Enable/disable logs on the PlayGamesPlatform
+            PlayGamesPlatform.DebugLogEnabled = GameConsts.PlayGamesDebugLogsEnabled;
+
+            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+                .EnableSavedGames()
+                .Build();
+            PlayGamesPlatform.InitializeInstance(config);
+
+            // Activate the Play Games platform. This will make it the default
+            // implementation of Social.Active
+            PlayGamesPlatform.Activate();
+
+            // Set the default leaderboard for the leaderboards UI
+            ((PlayGamesPlatform)Social.Active).SetDefaultLeaderboardForUI(GameIds.LeaderboardId);
+
+            // Sign in to Google Play Games
+            mAuthenticating = true;
+            Social.localUser.Authenticate((bool success) =>
+            {
+                mAuthenticating = false;
+                if (success)
+                {
+                // if we signed in successfully, load data from cloud
+                Debug.Log("Login successful!");
+                }
+                else
+                {
+                // no need to show error message (error messages are shown automatically
+                // by plugin)
+                Debug.LogWarning("Failed to sign in with Google Play Games.");
+                }
+            });
+        }
+
+        public void SignOut()
+        {
+            ((PlayGamesPlatform)Social.Active).SignOut();
+        }
+
+        public string AuthProgressMessage
+        {
+            get { return mAuthProgressMessage; }
+        }
+
+        public void ShowLeaderboardUI()
+        {
+            if (Authenticated)
+            {
+                Social.ShowLeaderboardUI();
+            }
+        }
+
+        public void ShowAchievementsUI()
+        {
+            if (Authenticated)
+            {
+                Social.ShowAchievementsUI();
+            }
+        }
+
+        public void PostToLeaderboard()
+        {
+            int score = 10;// mProgress.TotalScore;
+            if (Authenticated && score > mHighestPostedScore)
+            {
+                // post score to the leaderboard
+                Social.ReportScore(score, GameIds.LeaderboardId, (bool success) => { });
+                mHighestPostedScore = score;
+            }
+            else
+            {
+                Debug.LogWarning("Not reporting score, auth = " + Authenticated + " " +
+                                 score + " <= " + mHighestPostedScore);
+            }
+        }
+    }
+}
--- Assets/Scripts/Scenemanagement.cs
+++ Assets/Scripts/Scenemanagement.cs
@@ -1,11 +1,173 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GameLogic;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Scenemanagement : MonoBehaviour
 {
+    public AudioClip UiBeepFx;
+    public GameObject playButton;
+    public GameObject signinButton;
+    public GameObject signinMessage;
+    public GameObject achievementButton;
+    public GameObject highScoreButton;
+    public GameObject loadButton;
+    public GameObject saveButton;
+    public GameObject levelSelectionPanel;
 
+
+    static bool sAutoAuthenticate = true;
+    #region Authenticate
+    // Use this for initialization
+    void Start()
+    {
+        // if this is the first time we're running, bring up the sign in flow
+        if (sAutoAuthenticate)
+        {
+            GoogleServicesManager.Instance.Authenticate();
+            sAutoAuthenticate = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool standBy = GoogleServicesManager.Instance.Authenticating;
+        bool authenticated = GoogleServicesManager.Instance.Authenticated;
+
+        Text msg = signinMessage.GetComponent<Text>();
+        Button signin = signinButton.GetComponent<Button>();
+
+        if (standBy)
+        {
+            msg.text = Strings.SigningIn;
+            signin.interactable = false;
+        }
+        else if (authenticated)
+        {
+            msg.text = Strings.SignedInBlurb;
+            signin.interactable = !levelSelectionPanel.activeSelf;
+            signinButton.GetComponentInChildren<Text>().text = Strings.SignOut;
+        }
+        else
+        {
+            msg.text = Strings.SignInBlurb;
+            signin.interactable = !levelSelectionPanel.activeSelf;
+            signinButton.GetComponentInChildren<Text>().text = Strings.SignIn;
+        }
+
+        //achievementButton.SetActive(authenticated);
+        //highScoreButton.SetActive(authenticated);
+        //loadButton.SetActive(authenticated);
+        //saveButton.SetActive(authenticated);
+    }
+
+    public void OnSignIn()
+    {
+        if (GoogleServicesManager.Instance.Authenticating)
+        {
+            return;
+        }
+
+        Beep();
+
+        if (GoogleServicesManager.Instance.Authenticated)
+        {
+            Beep();
+            GoogleServicesManager.Instance.SignOut();
+        }
+        else
+        {
+            GoogleServicesManager.Instance.Authenticate();
+        }
+    }
+
+    public void OnPlay()
+    {
+        Beep();
+
+        if (GoogleServicesManager.Instance.Authenticated)
+        {
+            // If level 0 is the only possibility, don't bother to show the
+            // level selection screen, just go straight into the level.
+            //GameManager.Instance.GoToLevel(0);
+            SceneManager.LoadScene("SoldierShootScene1");
+        }
+        else
+        {
+            // Show the level selection screen
+            // gameObject.
+            // this.enabled = false;
+            //ShowLevelSelection();
+        }
+    }
+    #endregion
+
+    /*
+    public void OnLoadProgress()
+    {
+        Beep();
+        //GoogleServicesManager.Instance.LoadFromCloud();
+    }
+
+    public void OnSaveProgress()
+    {
+        Beep();
+        //GoogleServicesManager.Instance.SaveProgress();
+    }
+
+    void ShowLevelSelection()
+    {
+        levelSelectionPanel.SetActive(true);
+
+        playButton.GetComponent<Button>().interactable = false;
+        achievementButton.GetComponent<Button>().interactable = false;
+        highScoreButton.GetComponent<Button>().interactable = false;
+        signinButton.GetComponent<Button>().interactable = false;
+        loadButton.GetComponent<Button>().interactable = false;
+
+
+        Button[] levels = levelSelectionPanel.GetComponentsInChildren<Button>();
+        Text[] texts = levelSelectionPanel.GetComponentsInChildren<Text>();
+        for (int i = 0; i < levels.Length; i++)
+        {
+            // create new local var for closure for click listener
+            int level = i;
+            texts[i].text = "Sector " + Convert.ToChar('A' + i) +
+                            "\n" + GoogleServicesManager.Instance.Progress.GetLevelProgress(i).Score;
+            levels[i].interactable =
+                GoogleServicesManager.Instance.Progress.IsLevelUnlocked(i);
+            levels[i].onClick.AddListener(() =>
+            {
+                Debug.Log("Playing level " + level);
+                GoogleServicesManager.Instance.GoToLevel(level);
+            });
+        }
+    }
+
+    public void OnAchievements()
+    {
+        Beep();
+        //GoogleServicesManager.Instance.ShowAchievementsUI();
+    }
+
+    public void OnHighScores()
+    {
+        Beep();
+        //GoogleServicesManager.Instance.ShowLeaderboardUI();
+    }
+    */
+
+    void Beep()
+    {
+        AudioSource.PlayClipAtPoint(UiBeepFx, Vector3.zero);
+    }
+
+
+
+    #region BasicButtons
     public void StartGame()
     {
         SceneManager.LoadScene("SoldierShootScene1");
@@ -20,4 +182,5 @@ public class Scenemanagement : MonoBehaviour
     {
         Application.Quit();
     }
+    #endregion
 }
